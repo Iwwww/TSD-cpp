617efc90ed5ddd4f591b74bfc77410d3fee27176
Iwwww <mike.yolgin@gmail.com>
2022-09-27 17:48:49 +0300

    Add File class

---
+++ src/File/File.cpp
@@ -0,0 +1,77 @@
+#include "File.hpp"
+
+namespace YMM {
+    File::File() {}
+
+    File::File(std::string file_name, std::string mode) {
+        open(file_name, mode);
+    }
+
+    File::~File() {}
+
+    void File::setFileName(std::string file_name) {
+        this->file_name = file_name;
+    }
+
+    void File::initStream() {
+        if (this->isOpen()) {
+            this->out->close();
+            this->exception.warning("Force close file stream");
+        } else {
+            std::fstream out;
+            this->out = &out;
+        }
+    }
+
+    void File::open(std::string file_name, std::string _mode) {
+        this->setFileName(file_name);
+        this->initStream();
+
+        if (_mode == "r") 
+            this->mode = std::ios::in;
+        else if (_mode == "w")
+            this->mode = std::ios::out;
+        else if (_mode == "rw")
+            this->mode =  std::ios::in | std::ios::out;
+        else
+            this->mode = std::ios::in;
+            this->exception.error("Invalid mode: " + _mode);
+
+        this->out->open(file_name, this->mode);
+    }
+
+    void File::close() {
+        if (isOpen()) {
+            this->out->close();
+        }
+    }
+
+    bool File::isOpen() {
+        bool flag = 0;
+        if (this->out != nullptr) {
+            if (this->out->is_open()) {
+                flag = 1;
+            }
+        } else {
+            this->exception.warning("Empty Stream Ptr");
+        }
+
+        return flag;
+    }
+
+    void File::write(std::string text) {
+        *out << text;
+    }
+
+    std::string File::read() {
+        std::string text{};
+        char line[100];
+        if (this->mode == std::ios::in || this->mode == std::ios::ate) {
+            while (this->out->getline(line, 100)) {
+                text += line;
+            }
+        } else {
+            this->exception.error(&"Invalid reading mode: " [this->mode]);
+        }
+    }
+}
---
+++ src/File/File.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <fstream>
+#include <ios>
+#include <string>
+#include "../Exception/Exception.hpp"
+
+namespace YMM {
+    class File {
+        public:
+            File();
+            File(std::string, std::string);
+            ~File();
+
+            void setFileName(std::string);
+            void open(std::string file_name, std::string mode);
+            void close();
+            bool isOpen();
+            void write(std::string);
+            std::string read();
+
+            std::ostream& getStream();
+
+        private:
+            std::string file_name{};
+            std::fstream* out = nullptr;
+            std::ios::openmode mode{};
+            Exception exception;
+
+            void initStream();
+    };
+}
