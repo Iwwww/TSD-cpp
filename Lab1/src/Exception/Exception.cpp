ddcbab0999353322e7d88563d372fe6d23b7cb5f
Iwwww <mike.yolgin@gmail.com>
2022-10-11 01:29:02 +0300

    Make Exception complitly static class

--- Lab1/src/Exception/Exception.cpp
+++ Lab1/src/Exception/Exception.cpp
@@ -1,46 +1,33 @@
 #include "Exception.hpp"
-#include <string>
 
 namespace YMM {
-    Exception::Exception(std::string error) {
-        this->error_msg = error;
-        logging(this->error_msg);
-    }
-
-    Exception::Exception(std::string error, std::string file_name) {
-        setFileName(file_name);
-        this->error_msg = error;
-        logging(this->error_msg);
-    }
-
-    Exception::Exception() {}
-
-    Exception::~Exception() {}
-
     void Exception::logging(std::string msg, std::string log_type) {
         auto now = std::chrono::system_clock::now();
         time_t end_time = std::chrono::system_clock::to_time_t(now);
         std::string line = std::string("[LOG] << " + log_type + ": " + std::ctime(&end_time) + " " + msg.c_str() + "\n");
 
-        File file(this->file_name, "append");
+        File file(log_file_name, "append");
         file.write(line);
         file.close();
-
-    }
-
-    void Exception::logging(std::string msg) {
-        logging(msg, log_type_error);
     }
 
     void Exception::error(std::string msg) {
-        logging(msg, log_type_error);
+        logging(msg, std::string("ERROR"));
     }
 
     void Exception::warning(std::string msg) {
-        logging(msg, log_type_warning);
+        logging(msg, std::string("WARN"));
     }
 
-    void Exception::setFileName(std::string file_name) {
-        this->file_name = file_name;
+    void Exception::info(std::string msg) {
+        logging(msg, std::string("INFO"));
+    }
+
+    void Exception::setLogFileName(std::string file_name) {
+        log_file_name = file_name;
+    }
+
+    std::string Exception::getLogFileName() {
+        return log_file_name;
     }
 }
--- Lab1/src/Exception/Exception.hpp
+++ Lab1/src/Exception/Exception.hpp
@@ -1,28 +1,20 @@
 #pragma once
+#include "../File/File.hpp"
 #include <string>
 #include <chrono>
-#include "../File/File.hpp"
 
 namespace YMM {
     class Exception {
         public:
-            Exception(std::string);
-            Exception(std::string, std::string);
-            Exception();
-            ~Exception();
-            
-            void logging(std::string);
-            void logging(std::string, std::string);
-            void error(std::string);
-            void warning(std::string);
-            void setFileName(std::string);
-        
-        private:
-            std::string error_msg{};
-            std::string warning_msg{};
-            std::string file_name = "log.log";
-            std::string log_type_error = "ERROR";
-            std::string log_type_warning = "WARN";
+            static void logging(std::string, std::string);
+            static void error(std::string);
+            static void warning(std::string);
+            static void info(std::string);
 
+            static void setLogFileName(std::string file_name);
+            static std::string getLogFileName();
+
+        private:
+            static std::string log_file_name;
     };
 }
