1258fac9e5a9e06557601009ad81b84d60aea8a1
Iwwww <mike.yolgin@gmail.com>
2022-10-11 01:32:48 +0300

    Add Exceptions for input template function

--- Lab1/src/Menu/Menu.hpp
+++ Lab1/src/Menu/Menu.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include "../Exception/Exception.hpp"
 #include <iostream>
 #include <string>
 #include <any>
 #include <vector>
 
 namespace YMM {
+
     class Menu {
         public:
             using func = void(*)(std::any*);
@@ -16,13 +18,27 @@ namespace YMM {
             void printMenu();
             std::string getLable();
 
+            template<typename T>
+            static T input() {
+                T num = 0;
+                bool flag = true;
+                while (flag) {
+                    if (std::cin >> num) {
+                        flag = false;
+                    } else {
+                        Exception::warning("Invalid Input");
+                        std::cin.clear();
+                        while (std::cin.get() != '\n');
+                    }
+                }
+
+                return num;
+            }
 
         private:
             std::string lable{};
             std::vector<Menu> menus{};
             func function = nullptr;
             std::any* params = nullptr;
-            int level = 0;
-
     };
 }
