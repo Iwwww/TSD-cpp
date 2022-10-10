dfc3f56ec0c16df35cfd2dddd548bc21bbf88a2a
Iwwww <mike.yolgin@gmail.com>
2022-10-10 15:29:37 +0300

    Add Menu

--- Lab1/src/Menu/Menu.cpp
+++ Lab1/src/Menu/Menu.cpp
@@ -1,25 +1,71 @@
 #include "Menu.hpp"
+#include <chrono>
+#include <iterator>
+#include <string>
+#include <vector>
 
 namespace YMM {
-    Menu::Menu(std::string lable, func function, std::any params) {
-        this->lable = lable;
+    Menu::Menu(std::string lable, func function, std::any* params) { this->lable = lable;
         this->function = function;
         this->params = params;
     }
 
-    Menu::Menu(std::string lable, Menu menu, std::any params) {
+    Menu::Menu(std::string lable, std::vector<Menu> menus, std::any* params) {
         this->lable = lable;
-        *this->menu = menu;
+        this->menus = menus;
         this->params = params;
     }
 
+    Menu::Menu(const Menu& menu) {
+        this->lable = menu.lable;
+        this->menus = menu.menus;
+        this->function = *&menu.function;
+        this->params = menu.params;
+    }
+
     void Menu::run() {
-        if (this->function) {
-            return this->function(params);
-        } 
+        bool flag = true;
+        while (flag) {
+            int select{};
+            printMenu();
+
+            if (this->function == nullptr) {
+                do {
+                    std::cout << "Select >> ";
+                    std::cin >> select;
+                } while (select == -1 || select > menus.size());
+
+                select--;
+                std::cout << std::endl;
+
+                if (select == -1) {
+                    // exit
+                    flag = false;
+                } else {
+                    menus[select].run();
+                }
+
+            } else {
+                // run function
+                function(this->params);
+                flag = false;
+            }
+        }
     }
 
     void Menu::printMenu() {
-        std::cout << lable << std::endl;
+        if (this->function == nullptr) {
+            std::cout << "\n\n";
+            std::cout << "MENU:" << this->getLable() << std::endl;
+            std::cout << "**====**" << std::endl;
+        }
+
+        for (int i = 0; i < this->menus.size(); i++) {
+            std::cout << i + 1 << ". " << this->menus[i].getLable() << std::endl;
+        }
+    }
+
+    std::string Menu::getLable() {
+        return this->lable;
     }
 }
--- Lab1/src/Menu/Menu.hpp
+++ Lab1/src/Menu/Menu.hpp
@@ -1,22 +1,28 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <any>
+#include <vector>
 
 namespace YMM {
     class Menu {
         public:
-            using func = void(*)(std::any);
-            Menu(std::string lable, func, std::any);
-            Menu(std::string lable, Menu, std::any);
+            using func = void(*)(std::any*);
+            Menu(std::string lable, func function, std::any* params);
+            Menu(std::string lable, std::vector<Menu> menus, std::any* params);
+            Menu(const Menu& menu);
 
             void run();
             void printMenu();
+            std::string getLable();
+
 
         private:
             std::string lable{};
-            func function{};
-            Menu* menu{};
-            std::any params;
+            std::vector<Menu> menus{};
+            func function = nullptr;
+            std::any* params = nullptr;
+            int level = 0;
 
     };
 }
